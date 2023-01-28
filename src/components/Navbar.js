b6cab02209d45618a598266fa47f3444b11ae8d9
Israel CHIZUNGU <chizunguisrael@gmail.com>
2023-01-28 16:57:06 +0200

    set basic page structure

---
+++ src/components/Navbar.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Navbar = ()=>{
+    return (
+        <div className='navbar-container'>
+            <div className='navbar-left-align'>
+                <h1 className='logo'><Link className='navbar-logo-link' to='/'>Bookstore CMS</Link></h1>
+                <ul className='navbar-ul'>
+                    <li className='navbar-ul-li'><Link className='navbar-li-item' to='/books'>Books</Link></li>
+                    <li className='navbar-ul-li'><Link className='navbar-li-item' to='/books'>Category</Link></li>
+                </ul>
+            </div>
+            {/* avatar */}
+            <div className='navbar-right-align'>
+
+            </div>
+        </div>
+    );
+}
+
+export default Navbar
