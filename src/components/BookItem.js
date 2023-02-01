95e5d7968a333b31b9aaf82e75658ed525577442
Israel CHIZUNGU <chizunguisrael@gmail.com>
2023-02-01 23:45:31 +0200

    Add delete and insert books feature

--- src/components/BookItem.js
+++ src/components/BookItem.js
@@ -1,12 +1,17 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { removeBook } from '../redux/books/books';
 
-const BookItem = (props) => (
-    <div style={{ padding: 20, backgroundColor: '#efefef', width: 700, marginBottom: 5 }}>
-        <h2>{props.book.title}</h2>
-        <h3>{props.book.author}</h3>
-        <button>Edit</button>
-        <button>Delete</button>
-    </div>
-);
+const BookItem = (props) => {
+    const dispatch = useDispatch();
+    return (
+        <div style={{ padding: 20, backgroundColor: '#efefef', width: 700, marginBottom: 5 }}>
+            <h2>{props.book.title}</h2>
+            <h3>{props.book.author}</h3>
+            <button>Edit</button>
+            <button onClick={() => dispatch(removeBook(props.book.id))}>Delete</button>
+        </div>
+    )
+};
 
 export default BookItem
